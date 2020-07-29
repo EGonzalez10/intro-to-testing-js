5f0c9091b1e79ca4d1953d274a382125455491d6
Edgar Gonzalez <gonzalez.edgar10@gmail.comm>
2020-07-29 11:25:19 -0500

    update

--- code.js
+++ code.js
@@ -3,5 +3,11 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
+function sayHello(name) {
+    if (name === "Alex") {
+        return "Hello, Alex!";
+    } else {
+        return "Hello, Jane!";
+    }
 }
+
